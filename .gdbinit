4288b96e351e3f72791bb8e8fbbd0292776d602b
Herman Sahota <herman@cerebras.net>
2017-07-17 13:03:59 -0700

    Add gdbinit file.

---
+++ .gdbinit
@@ -0,0 +1,6 @@
+set history save on
+#set history remove-duplicates unlimited
+set print pretty on
+set print elements 9216*4096
+set print array on
+set pagination off
--- bootstrap.sh
+++ bootstrap.sh
@@ -2,7 +2,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files=".bashrc .vimrc .bash_profile .bash_herman .bash_cerebras .git-completion.bash .git-prompt.sh"        # list of files/folders to symlink in homedir
+files=".gdbinit .bashrc .vimrc .bash_profile .bash_herman .bash_cerebras .git-completion.bash .git-prompt.sh"        # list of files/folders to symlink in homedir
 
 ##########
 
