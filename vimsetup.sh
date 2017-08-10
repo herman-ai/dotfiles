16cf3b1bb00bd52361ee1ea44c1ef4912739cce7
Herman Sahota <herman@cerebras.net>
2017-08-10 14:28:52 -0700

    Change path for deletion.

--- vimsetup.sh
+++ vimsetup.sh
@@ -1,4 +1,4 @@
 mkdir -p ~/.vim/autoload ~/.vim/bundle
-rm -rf /Users/herman/.vim/bundle/Vundle.vim
+rm -rf  ~/.vim/bundle/Vundle.vim
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
