f5285c3d323286aee90c67a3fa1bc3e6eec0aa15
Herman Sahota <herman@cerebras.net>
2017-08-10 14:34:45 -0700

    Set up symlink to .vim from home.

--- vimsetup.sh
+++ vimsetup.sh
@@ -1,3 +1,5 @@
+mkdir .vim
+ln -s .vim ~/.vim
 mkdir -p ~/.vim/autoload ~/.vim/bundle
 rm -rf  ~/.vim/bundle/Vundle.vim
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
