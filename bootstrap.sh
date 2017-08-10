6f75624fe32b09307f142d30c8be764aa6711963
Herman Sahota <herman@cerebras.net>
2017-08-10 14:03:10 -0700

    Clean up.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,6 @@
 ########## Variables
 
-dir=~/dotfiles                    # dotfiles directory
+dir=~/ws/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
 files=".gdbinit .bashrc .vimrc .bash_profile .bash_herman .bash_cerebras .git-completion.bash .git-prompt.sh"        # list of files/folders to symlink in homedir
 
@@ -19,8 +19,10 @@ echo "...done"
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks
 for file in $files; do
     echo "Moving any existing dotfiles from ~ to $olddir"
+    echo $file
     mv ~/$file ~/dotfiles_old/
     echo "Creating symlink to $file in home directory."
     ln -s $dir/$file ~/$file
+    echo "...done"
 done
 source ~/.bashrc
